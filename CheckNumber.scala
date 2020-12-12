f14499ca637385d811797245cb36efb3cb79ae9d
SriMadhumithaK <74954002+SriMadhumithaK@users.noreply.github.com>
2020-12-12 20:15:34 +0530

    Create CheckNumber.scala

---
+++ CheckNumber.scala
@@ -0,0 +1,16 @@
+object CheckNumber {
+       def main(args: Array[String]) {
+               
+            var number= (-23);
+       
+           if(number==0){
+               println("number is zero");
+           }
+           else if(number>0){
+               println("number is positive");
+           }
+           else{
+               println("number is negative");
+           }
+       }
+    }
